f4e7c46dc65629b7e647d9e7174a6a025cf148cf
yusniel Espino <espvtes@gmail.com>
2019-05-15 23:42:26 -0400

    styles

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import React, {Component}from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar} from './components/Navbar.js';
-import ProductList from './components/ProductList';
-import Details from './components/Details.js';
-import Cart from './components/Cart.js';
-import Default from './components/Default.js';
 
 
-export class App extends React.Component{
+class App extends Component{
   render(){
   return (
-    <React.Fragment>
-      <Navbar/>
-      <Switch>
-        <Route exact path="/" components={ProductList}/>
-        <Route path="/details" components={Details}/>
-        <Route path="/cart" components={Cart}/>
-        <Route components={Default}/>
-      </Switch>
-    </React.Fragment>
+   <div className="container">
+     <div className="row">
+       <div className="col-6">column number one</div>
+       <div className="col-6"><span><i className="fas fa-home" ></i></span></div>
+     </div>
+   </div>
     
   );
  }
